443b2ba3e929b6dfed2c9beaa10aee1e76dd94c2
BlixtSamme <samuel.hogfeldt@gmail.com>
2022-03-03 11:28:34 +0100

    Initial Commit

---
+++ Swift presentation/Swift_presentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_presentationApp.swift
+//  Swift presentation
+//
+//  Created by Samuel Högfeldt on 2022-03-03.
+//
+
+import SwiftUI
+
+@main
+struct Swift_presentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
